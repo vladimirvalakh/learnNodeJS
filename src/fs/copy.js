73c5e355304628516c5429fc2c58b38df6dc06d4
Vladimir Valakh <sinclair.ubuntu@gmail.com>
2022-12-01 17:50:53 +0300

    implement create and copy functionality

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import path from 'path'
+import {fileURLToPath} from 'url';
+
 const copy = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const folderPath = path.join(__dirname, 'files');
+    const copyFolderPath = path.join(__dirname, 'files_copy');
+
+    if (fs.existsSync(copyFolderPath)) {
+        throw Error('FS operation failed')
+    } else {
+        fs.cp(folderPath, copyFolderPath, {recursive: true}, function (err) {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log("success!");
+            }
+        });
+    }
 };
 
 copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import {open} from 'node:fs';
+import fs from 'fs';
+import path from 'path'
+import {fileURLToPath} from 'url';
+
 const create = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const filePath = path.join(__dirname, 'files', 'fresh.txt');
+
+    open(filePath, 'wx', (err) => {
+        if (err && err.code === 'EEXIST') {
+            throw Error('FS operation failed')
+        }
+
+        fs.writeFile(filePath, 'I am fresh and young', function (err) {
+            if (err) throw err;
+            console.log('File is created successfully.');
+        });
+    });
 };
 
 await create();
