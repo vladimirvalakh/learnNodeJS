0347a97f9225e990f23ee180637c776e3de4d8b1
Vladimir Valakh <sinclair.ubuntu@gmail.com>
2022-12-05 18:25:58 +0300

    implement compress functionality

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+import {createGzip} from 'node:zlib';
+import pipeline from 'node:stream';
+
+import fs from 'node:fs';
+import path from 'path'
+import {fileURLToPath} from 'url';
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
 const compress = async () => {
-    // Write your code here 
+    const filenamePath = path.join(dirname, 'files', 'fileToCompress.txt');
+    const filenameZipPath = path.join(dirname, 'files', 'archive.gz');
+    const gzip = createGzip();
+    const source = fs.createReadStream(filenamePath);
+    const destination = fs.createWriteStream(filenameZipPath);
+
+    pipeline(source, gzip, destination, (err) => {
+        if (err) {
+            console.error('Error:', err);
+            process.exitCode = 1;
+        }
+    });
 };
 
 await compress();
